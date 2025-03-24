e3b9618136fbfd12d4047a49c5d9b95b9e54e33c
miyakei1225 <miyakeionigiri@yahoo.co.jp>
2025-03-24 16:13:52 +0900

    Add: tsconfig settings

--- hono-server/dist/index.js
+++ hono-server/dist/index.js
@@ -1,14 +1,19 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-var node_server_1 = require("@hono/node-server");
-var hono_1 = require("hono");
-var app = new hono_1.Hono();
-app.get("/", function (c) {
+const node_server_1 = require("@hono/node-server");
+const hono_1 = require("hono");
+const vision_1 = __importDefault(require("./routes/vision"));
+const app = new hono_1.Hono();
+app.get("/", (c) => {
     return c.text("Hello Hono!");
 });
-var port = 8080;
-console.log("Server running at http://localhost:".concat(port));
+app.route("/vision", vision_1.default);
+const port = 8080;
+console.log(`Server running at http://localhost:${port}`);
 (0, node_server_1.serve)({
     fetch: app.fetch,
-    port: port,
+    port,
 });
